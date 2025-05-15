627d574dcfc1a5ab9db774344decc97dfef2efb0
Polcia <witkowska.pola@gmail.com>
2025-05-15 21:51:43 +0200

    implemented getting parameters

--- main.go
+++ main.go
@@ -6,47 +6,82 @@ import (
 )
 
 type forest struct {
-	isTree    bool
-	isBurning bool
-	isBurned  bool
+	isTree   bool
+	isBurned bool
 }
 
-func makeForest(n int) [][]forest {
-	grid := make([][]forest, n)
-	for i := range grid {
-		grid[i] = make([]forest, n)
+func getParameters() [3]int {
+	var parameters [3]int
+	fmt.Print("Enter X, Y and P:\n")
+	_, err := fmt.Scanln(&parameters[0], &parameters[1], &parameters[2])
+	if err != nil {
+		fmt.Println("Error while getting parameters. Try again.")
+		return [3]int{0, 0, 0}
 	}
 
-	for i := 0; i < len(grid); i++ {
-		for j := 0; j < len(grid[0]); j++ {
-			grid[i][j] = forest{
-				isTree:    rand.Intn(2) == 1,
-				isBurning: false,
-				isBurned:  false,
-			}
-		}
+	if parameters[0] <= 0 || parameters[1] <= 0 || parameters[2] <= 0 {
+		fmt.Println("Parameters X, Y and P cannot be 0 or negative! Try again.")
+		return [3]int{0, 0, 0}
 	}
+
+	//if parameters[2] > 100 {
+	//	fmt.Println("P must be less or equal to 100!")
+	//	return [3]int{0, 0, 0}
+	//}
+
+	return parameters
+}
+
+func makeForest(x int, y int, p int) [][]forest {
+	grid := make([][]forest, x)
+	for i := range grid {
+		grid[i] = make([]forest, y)
+	}
+
+	howManyTrees := (float64(x) * float64(y)) * (float64(p) / 100)
+
+	for a := range int(howManyTrees) {
+		i := rand.Intn(x)
+		j := rand.Intn(y)
+		fmt.Println(a)
+
+		if !grid[i][j].isTree {
+			grid[i][j].isTree = true
+		} else {
+			howManyTrees++
+		}
+
+	}
+
+	//for i := range grid {
+	//	for j := range grid[i] {
+	//		grid[i][j] = forest{
+	//			isTree:   rand.Intn(2) == 1,
+	//			isBurned: false,
+	//		}
+	//	}
+	//}
 
 	return grid
 }
 
-func thunder(n int, grid [][]forest) {
-	targetX := rand.Intn(n)
-	targetY := rand.Intn(n)
+func thunder(x int, y int, grid [][]forest) {
+	targetX := rand.Intn(x)
+	targetY := rand.Intn(y)
 
 	fmt.Println("\nTarget:", targetX, targetY)
 
 	if !grid[targetX][targetY].isTree {
-		fmt.Println("Target was empty")
-		return
+		fmt.Println("Target was empty. Repeating.")
+		thunder(x, y, grid)
 	}
 
-	grid[targetX][targetY].isBurning = true
+	grid[targetX][targetY].isBurned = true
 
-	for _ = range n {
-		for i := range n {
-			for j := range n {
-				if grid[i][j].isTree && grid[i][j].isBurning && !grid[i][j].isBurned {
+	for _ = range x {
+		for i := range x {
+			for j := range y {
+				if grid[i][j].isTree && grid[i][j].isBurned {
 					directions := [][2]int{
 						{-1, 0}, {1, 0}, {0, -1}, {0, 1},
 						{-1, -1}, {-1, 1}, {1, -1}, {1, 1},
@@ -54,30 +89,50 @@ func thunder(n int, grid [][]forest) {
 
 					for _, direction := range directions {
 						newI, newJ := i+direction[0], j+direction[1]
-						if newI >= 0 && newI < n && newJ >= 0 && newJ < n &&
-							grid[newI][newJ].isTree && !grid[newI][newJ].isBurning {
-							grid[newI][newJ].isBurning = true
+						if newI >= 0 && newI < x && newJ >= 0 && newJ < y &&
+							grid[newI][newJ].isTree && !grid[newI][newJ].isBurned {
+							grid[newI][newJ].isBurned = true
 						}
 					}
-					grid[i][j].isBurned = true
 				}
 			}
 		}
+		printForest(grid)
 	}
 }
 
+func printForest(grid [][]forest) {
+	for _, row := range grid {
+		fmt.Println(" ")
+		for _, cell := range row {
+			if cell.isTree && !cell.isBurned {
+				fmt.Print("T ")
+			} else if cell.isTree && cell.isBurned {
+				fmt.Print("B ")
+			} else {
+				fmt.Print("_ ")
+			}
+		}
+	}
+	fmt.Println(" ")
+}
+
 func main() {
-	n := 6
-	grid := makeForest(n)
+	parameters := getParameters()
 
-	fmt.Println("Forest: ")
-	for i := range grid {
-		fmt.Println(grid[i])
+	if parameters[0] == 0 && parameters[1] == 0 && parameters[2] == 0 {
+		return
 	}
 
-	thunder(n, grid)
-	fmt.Println("Forest after thunder: ")
-	for i := range grid {
-		fmt.Println(grid[i])
-	}
+	x := parameters[0]
+	y := parameters[1]
+	p := parameters[2]
+
+	grid := makeForest(x, y, p)
+	fmt.Print("Forest: ")
+	printForest(grid)
+
+	//thunder(x, y, grid)
+	//fmt.Println("Forest after thunder: ")
+	//printForest(grid)
 }
